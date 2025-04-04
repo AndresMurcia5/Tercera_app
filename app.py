ebb736143395c04ce29c9a634af4697036c8527f
Sala 203 <sala203@ujaverianas.loc>
2025-04-04 15:02:11 -0500

    second commit

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ st.title("Esto es una app")
 year = st.selectbox("Seleccione un año",[2024,2023,2022])
 
 if year == 2024:
-    df_m.read_parquet("hombres24.parquet")
-    df_f.read_parquet("mujeres24.parquet")
+    df_m = gpd.read_parquet("hombres24.parquet")
+    df_f = gpd.read_parquet("mujeres24.parquet")
 elif year == 2023:
-    df_m.read_parquet_parquet("hombres23.parquet")
-    df_f.read_parquet_parquet("mujeres23.parquet")
+    df_m = gpd.read_parquet("hombres23.parquet")
+    df_f = gpd.read_parquet("mujeres23.parquet")
 else:
-    df_m.read_parquet_parquet("hombres22.parquet")
-    df_f.read_parquet_parquet("mujeres22.parquet")
+    df_m = gpd.read_parquet("hombres22.parquet")
+    df_f = gpd.read_parquet("mujeres22.parquet")
 fig, ax = plt.subplots(1, 2, figsize=(10, 6))
 
 df_m.plot(column='FT', ax=ax[0], legend=True, vmin=0.2,vmax=1)
